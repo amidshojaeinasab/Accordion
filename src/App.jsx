b2153227cf88e90a244a9600a3244f3e34dfa898
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-10-16 10:14:47 +0330

    optimize the code

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Question from './question'
 import data from "./Data"
 
 function App() {
-const [questions, setQuestions] = useState (data)
+
 
   return (
     <div className="container">
       <h3>سوالات متداول</h3>
       <div className="info">
         {
-          questions.map((question) =>{
+          data.map((question) =>{
             return <Question key={question.id} {...question}/>
           })
         }
